f4f75202a2455321c0484feb55b4c365b011efeb
Nic <nicjorgi@gmail.com>
2018-02-08 12:09:39 -0700

    First Commit

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,19 +1,34 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(draw); //after get music by artist returns what are you doing with the objects?
+    itunesService.getMusicByArtist(artist).then(drawSongs); //after get music by artist returns what are you doing with the objects?
   }
 
-  //Start coding here
-  function draw(results){
-    console.log(results)
+  // Start coding here
+  function drawSongs(songList) {
+    var template = ''
+    var songs = document.getElementById('songs')
+    for (let i = 0; i < 30; i++) {
+      var song = songList[i];
+      template += `
+      <div class="col-md-3 col-sm-6">
+         <div class="text-center bg-1">
+          <img src="${song.albumArt}" class="albumArt">
+            <h3>${song.title}</h3>
+            <audio controls>
+            <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+            <h6>${song.collection}</h6>
+            <h5>${song.artist}</h5>
+            <p>$${song.price}</p>
+          </div>
+      </div>
+      `
+      songs.innerHTML = template
+    }
   }
-
-
-
-
-  
 }
+
